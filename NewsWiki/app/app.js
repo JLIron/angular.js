6050975ac6949cd56973691c89f532bda9d8f3a7
JLIron <jlhm2012@gmail.com>
2019-10-20 22:37:54 +0200

    Solucionado error url: "/noticias/:idNoticia"

--- NewsWiki/app/app.js
+++ NewsWiki/app/app.js
@@ -24,28 +24,7 @@
                 controller: "NewsCtrl as vm"
             })
             .state("newsDetail", {
-                url: "/noticias/:idnoticia",
-                params: { 
-                    // here we define default value for foo
-                    // we also set squash to false, to force injecting
-                    // even the default value into url
-                    foo: {
-                      value: 'defaultValue',
-                      squash: false,
-                    },
-                    idnoticia: {
-                        value: '2',
-    
-                      },
-                    // this parameter is now array
-                    // we can pass more items, and expect them as []
-                    bar : { 
-                      array : true,
-                    },
-                    // this param is not part of url
-                    // it could be passed with $state.go or ui-sref 
-                    hiddenParam: 'YES',
-                  },
+                url: "/noticias/:idNoticia",
                 templateUrl: "app/news/newsDetailView.html",
                 controller: "NewsDetailCtrl as vm",
                 resolve:{
